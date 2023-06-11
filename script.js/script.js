2ab596f4e2a1394ed6c31745c75b911e6eb32f73
Daniel Lira <dnllira1@gmail.com>
2023-06-11 13:18:01 -0300

    Refatoração do codigo

--- script.js/script.js
+++ script.js/script.js
@@ -1,9 +1,25 @@
 const chk = document.getElementById('chk');
+const localStorageKey = "dark";
 
 chk.addEventListener('change', () => {
-  document.body.classList.toggle('dark');
+  toggleDarkMode(chk.checked);
   saveDataToLocalStorage(localStorageKey, chk.checked);
 });
+
+initialize();
+
+function initialize() {
+  const data = loadDataFromLocalStorage(localStorageKey);
+
+  if (data !== null && typeof data !== 'undefined') {
+    chk.checked = data;
+    toggleDarkMode(data);
+  }
+}
+
+function toggleDarkMode(isDark) {
+  document.body.classList.toggle('dark', isDark);
+}
 
 function saveDataToLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
@@ -12,14 +28,6 @@ function saveDataToLocalStorage(key, data) {
 function loadDataFromLocalStorage(key) {
   const data = JSON.parse(localStorage.getItem(key));
   return data;
-}
-
-const localStorageKey = "dark";
-const data = loadDataFromLocalStorage(localStorageKey);
-
-if (data !== null && typeof data !== 'undefined') {
-  chk.checked = data;
-  document.body.classList.toggle('dark', data);
 }
 
 function save() {
