fa8a37e33cda4c0de119a0fa2dfd79d52fabff98
Daniel Lira <dnllira1@gmail.com>
2023-06-10 23:26:45 -0300

    Utiliza o localStorage para salvar e carregar o estado do tema escuro

--- script.js/script.js
+++ script.js/script.js
@@ -1,5 +1,27 @@
-const chk = document.getElementById('chk')
+const chk = document.getElementById('chk');
 
 chk.addEventListener('change', () => {
-  document.body.classList.toggle('dark')
-})
+  document.body.classList.toggle('dark');
+  saveDataToLocalStorage(localStorageKey, chk.checked);
+});
+
+function saveDataToLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+function loadDataFromLocalStorage(key) {
+  const data = JSON.parse(localStorage.getItem(key));
+  return data;
+}
+
+const localStorageKey = "dark";
+const data = loadDataFromLocalStorage(localStorageKey);
+
+if (data !== null && typeof data !== 'undefined') {
+  chk.checked = data;
+  document.body.classList.toggle('dark', data);
+}
+
+function save() {
+  saveDataToLocalStorage(localStorageKey, chk.checked);
+}
